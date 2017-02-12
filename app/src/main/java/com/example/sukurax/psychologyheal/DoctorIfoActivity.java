b8253ecc4dc72c1501e499084a13098fda4a1bfc
sukurax <1421578048@qq.com>
2017-02-12 20:30:16 +0800

    完成咨询，医生个人信息，订单界面

---
+++ app/src/main/java/com/example/sukurax/psychologyheal/DoctorIfoActivity.java
@@ -0,0 +1,40 @@
+package com.example.sukurax.psychologyheal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by sukurax on 2017/2/12.
+ */
+
+public class DoctorIfoActivity extends AppCompatActivity{
+    Button orderBtn;
+    ImageView doctorIfoBackBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctorifo);
+        orderBtn=(Button)findViewById(R.id.orderBtn);
+        doctorIfoBackBtn=(ImageView)findViewById(R.id.doctorIfoBackBtn);
+        orderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setClass(DoctorIfoActivity.this,OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+        doctorIfoBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setClass(DoctorIfoActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/sukurax/psychologyheal/LoginActivity.java
+++ app/src/main/java/com/example/sukurax/psychologyheal/LoginActivity.java
@@ -14,16 +14,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVOSCloud;
 import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.LogInCallback;
+import com.avos.avoscloud.RequestMobileCodeCallback;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText loginUsername;
-    EditText loginPassword;
+    EditText loginUsername,registerPhoneNumber,Captcha,loginPassword;
     TextView resetPassword;
-    String loginusername;
-    String loginpassword;
-    Button loginBtn;
+    String loginusername,loginpassword,registerphonenumber,captcha;
+    Button loginBtn,next,sendCaptchaBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +45,63 @@ public class LoginActivity extends AppCompatActivity {
         loginUsername=(EditText)findViewById(R.id.loginUsername);
         loginPassword=(EditText)findViewById(R.id.loginPassword);
         loginBtn=(Button)findViewById(R.id.loginBtn);
+
+        registerPhoneNumber=(EditText)findViewById(R.id.registerPhoneNumber);
+        Captcha=(EditText)findViewById(R.id.captcha);
+        sendCaptchaBtn=(Button)findViewById(R.id.sendCaptchaBtn);
+        next=(Button)findViewById(R.id.next);
+
+        sendCaptchaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerphonenumber=registerPhoneNumber.getText().toString().trim();
+                if(registerphonenumber.length()==0){
+                    Toast.makeText(getApplicationContext(), "输入不能为空！",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    AVOSCloud.requestSMSCodeInBackground(registerphonenumber, new RequestMobileCodeCallback() {
+                        @Override
+                        public void done(AVException e) {
+                            if(e==null){
+                                Toast.makeText(getApplicationContext(), "短信发送成功，请注意查收",
+                                        Toast.LENGTH_SHORT).show();
+                            }else {
+                                String temp[] = e.toString().split("\"");
+                                Toast.makeText(getApplicationContext(), temp[temp.length-2],
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerphonenumber=registerPhoneNumber.getText().toString().trim();
+                captcha=Captcha.getText().toString().trim();
+                if(registerphonenumber.length()==0||captcha.length()==0){
+                    Toast.makeText(getApplicationContext(), "输入不能为空！",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    AVUser.signUpOrLoginByMobilePhoneInBackground(registerphonenumber, captcha, new LogInCallback<AVUser>() {
+                        @Override
+                        public void done(AVUser avUser, AVException e) {
+                            if(e==null){
+                                Intent intent = new Intent();
+                                intent.putExtra("phoneNumber", registerphonenumber);
+                                intent.setClass(LoginActivity.this, registerActivity.class);
+                                startActivity(intent);
+                            }else{
+                                String temp[] = e.toString().split("\"");
+                                Toast.makeText(getApplicationContext(), temp[temp.length-2],
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,7 +139,7 @@ public class LoginActivity extends AppCompatActivity {
         resetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "点击了忘记密码",
+                Toast.makeText(getApplicationContext(), "点击忘记密码",
                         Toast.LENGTH_SHORT).show();
             }
         });
